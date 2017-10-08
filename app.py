358d0c7138db815d1d27c3d10916a565dadfd1be
gurraunaqsingh <raunaq.soni@gmail.com>
2017-10-08 06:22:42 +0530

    GG

--- app.py
+++ app.py
@@ -18,8 +18,8 @@ config = {
 
 app = Flask(__name__, static_url_path='/static')
 
-app.config['MONGO_DBNAME'] = 'abidbgac'
-app.config['MONGO_URI'] = 'mongodb://user1:abcd1234@ds159493.mlab.com:59493/abidbgac'
+app.config['MONGO_DBNAME'] = 'vistara'
+app.config['MONGO_URI'] = 'mongodb://user1:user1@ds113785.mlab.com:13785/vistara'
 
 mongo = PyMongo(app)
 
@@ -39,21 +39,102 @@ def dashboard():
 
 	return render_template('dashboard.html', analytics=analytics)
 
-app.route('/postfromapp', methods=['POST'])
+@app.route('/postfromapp', methods=['POST'])
 def post_from_app():
 	if request.method == "POST":
-		formdata = (request.form).to_dict()
+		formdata = (request.get_json())
 
 		#parse karna h isse
 		username = formdata['username']
 		route = formdata['route']
-		day = formdata['day']
+		date = formdata['date']
 		price = formdata['price']
 		choice = formdata['choice']
 		score = formdata['score']
 
 		#leaderboard me add kar
-		
+		usersdata = mongo.db.usersdata
+		data = usersdata.find_one({'maindata' : 'maindata'})
+
+		temp = {}
+		flag = 0
+
+		for item in data['data']:
+			if(item['username'] == username):
+				flag = 1
+				_history = []
+
+				#append
+				_history = item['history']
+				h = {"date" : date, "score" : score}
+				_history.append(h)
+
+				result = mongo.db.usersdata.update_one({"username" : item['username']}, {'$set' : {"username" : "lodu"}})
+
+		if(flag == 0):
+			#create
+			new_user = {}
+
+			new_user['username'] = formdata['username']
+			new_user['route'] = formdata['route']
+
+			history = []
+			h = {}
+			h['date'] = formdata['date']
+			h['price'] = formdata['price']
+			history.append(h)
+
+			new_user['choice'] = formdata['choice']
+			new_user['score'] = formdata['score']
+			new_user['history'] = history
+
+			data['data'].append(new_user)
+
+			result = mongo.db.usersdata.update_one({"maindata" : 'maindata'}, {'$set' : {"data" : data}})
+
+			return(jsonify(data['data']))
+
+		usersdata = mongo.db.usersdata
+		existing_user = usersdata.find_one({'username' : formdata['username']})
+
+		history = []
+
+		h = {"date" : date, "score" : score}
+		history.append(h)
+
+		result = mongo.db.usersdata.update_one({"username" : formdata['username']}, {'$set' : {"history" : history}})
+
+		print(result)
+
+		return ("GG")
+
+@app.route('/usersdata')
+def get_usersdata():
+	usersdata = mongo.db.usersdata
+
+	data = usersdata.find_one({'maindata' : 'maindata'})
+
+	leaderboard = []
+	for item in data['data']:
+		person = {}
+		person['username'] = item['username']
+		person['score'] = item['score']
+
+		leaderboard.append(person)
+
+	print(leaderboard)
+
+
+
+	return ("GG")
+
+def get_leaderboard(data):
+
+	return (leaderboard)
+
+			# users.insert({'name' : request.form['username'], 'password' : hashpass})
+			# session['username'] = request.form['username']
+			# return redirect(url_for('changes'))
 
 		#user rank return karni h
 
